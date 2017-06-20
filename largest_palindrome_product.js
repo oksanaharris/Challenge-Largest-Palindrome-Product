b76db829d03a10ac4f1be6239b33e607cb8c9d8b
Oksana Harr <oksana.harris@gmail.com>
2017-06-20 10:22:38 -1000

    finished but times out

--- largest_palindrome_product.js
+++ largest_palindrome_product.js
@@ -6,17 +6,56 @@
  *                   the palindromeNumber and the palindromeNumber itself.
  */
 module.exports = function(digits){
-  var factor_0 = 0;
-  var factor_1 = 0;
+  var factor_0;
+  var factor_1;
   var palindromeNumber = 0;
+  var palindromeArray = [];
+  var final_factor_0;
+  var final_factor_1;
 
-  // do your work here
+   // function generatePallindrome() {
+    var product;
+    var stringProd;
+    var stringArr;
 
+    // var startVal = Math.pow(10, (digits - 1));
+    // var finishVal = Math.pow(10, digits);
 
+    // console.log(startVal);
+    // console.log(finishVal);
+    
+    for (factor_0 = Math.pow(10, (digits - 1)); factor_0 < Math.pow(10, digits); factor_0++){
+      
+      for (factor_1 = Math.pow(10, (digits - 1)); factor_1 < Math.pow(10, digits); factor_1++){
+        
+        product = factor_0 * factor_1;
+        stringProd = product.toString();
+        stringArr = stringProd.split("");
+        reversedProd = stringArr.reverse().join("");
+        // console.log(stringProd);
+        // console.log(reversedProd);
+
+        if (stringProd === reversedProd){
+          // console.log('we have a palindrome!');
+          // palindromeArray.push(product);
+          if (product > palindromeNumber){
+            palindromeNumber = product;
+            final_factor_0 = factor_0;
+            final_factor_1 = factor_1;
+          }
+        }
+      }
+    }
+
+    factor_0 = final_factor_0;
+    factor_1 = final_factor_1;
+
+    console.log('Largest palindrome is ' + palindromeNumber);  
 
   return {
     factor_0 : factor_0,
     factor_1 : factor_1,
     palindromeNumber : palindromeNumber
   };
+
 };
